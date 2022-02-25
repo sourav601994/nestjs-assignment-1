caae5a413b59e9af5efc790e88cd618f016a38fe
sourav601994 <sourav.majumdar@neudesic.com>
2022-02-25 19:49:13 +0530

    Adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Assignment')
+    .setDescription('API specification')
+    .setVersion('1.0')
+    .addTag('User')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -13,7 +13,6 @@ export class UserService {
   ) {}
 
   getUsers(search?: string): Promise<User[]> {
-    console.log(search);
     return this.usersRepository.getUsers(search);
   }
 
